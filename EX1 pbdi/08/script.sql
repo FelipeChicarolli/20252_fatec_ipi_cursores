66956f64fbdb6dbc84c3b3946e58919a40cf1fc5
Ricardo Galdino <felipe.chicarolli.estudar@gmail.com>
2025-09-02 09:42:19 -0300

    learn(08): encerra a apostila

---
+++ EX1 pbdi/08/script.sql
@@ -0,0 +1,59 @@
+DO $$
+DECLARE
+    n1 NUMERIC(5,2);
+    n2 INTEGER;
+    limite_inferior INTEGER := 5;
+    limite_superior INTEGER := 17;
+BEGIN
+    n2 := limite_inferior + floor(random() * (limite_superior - limite_inferior + 1))::int;
+    RAISE NOTICE '% <= % <= %', limite_inferior, n2, limite_superior;
+    n2 := 4 + floor(random() * 6) ::INT;
+    RAISE NOTICE '4 <= % <= 9', n2;
+    n2 := floor(random() * 10 + 1)::INT;
+    RAISE NOTICE '1 <= % <= 10', n2;
+    n2 := FLOOR(random()*10)::INT;
+    RAISE NOTICE '0 <= % <= 9', n2;
+    n1 := random() * 10;
+    RAISE NOTICE '0 <= % < 10', n1;
+    n1 := random();
+    RAISE NOTICE '0 <= % < 1', n1;
+END;
+$$
+
+-- DO $$
+-- DECLARE
+--     n1 INTEGER := 5;
+--     n2 INTEGER := 2;
+--     n3 NUMERIC(5,2) := 5;
+--     n4 INTEGER := -5;
+-- BEGIN
+--     RAISE NOTICE '|/% = %', n1, |/n1;
+--     RAISE NOTICE '% %% % = %', n1, n2, n1 % n2;
+--     RAISE NOTICE '% / % = %', n1, n2, n1 / n2;
+--     RAISE NOTICE '% / % = %', n3, n2, n3 / n2;
+-- END;
+-- $$
+
+
+-- DO $$
+-- DECLARE
+--     codigo INTEGER := 1;
+--     nome_completo VARCHAR(200) := 'João Santos';
+--     salario NUMERIC (10,2) := 20.5;
+-- BEGIN
+--     RAISE NOTICE 'Meu código é %, me chamo % e meu salário é %', codigo, nome_completo, salario;
+-- END;
+-- $$
+
+-- DO $$
+-- BEGIN
+--     RAISE NOTICE '% + % = %', 6, 7, 6 + 7 ;
+-- END;
+-- $$ 
+
+-- DO $$
+-- BEGIN
+--     -- PARA EXIBIR VALORES NO CONSOLE
+--     RAISE NOTICE 'Meu primeiro bloquinho anônimo';
+-- END;
+-- $$
